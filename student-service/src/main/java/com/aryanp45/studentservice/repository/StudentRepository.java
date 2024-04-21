f70614b1835df324b51c2893c8d877a83e2a4199
Aryan Patil <aryanitinpatil@gmail.com>
2024-04-21 19:52:17 +0530

    Create StudentRepository.java

---
+++ student-service/src/main/java/com/aryanp45/studentservice/repository/StudentRepository.java
@@ -0,0 +1,14 @@
+package com.aryanp45.studentservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aryanp45.studentservice.model.Student;
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+	Optional<Student> findByPrn(Long prn);
+}

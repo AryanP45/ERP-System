37339e06da9762900b349395363304a963c70ebc
Aryan Patil <aryanitinpatil@gmail.com>
2024-04-21 20:45:21 +0530

    study material service setup

---
+++ study-material-service/src/main/java/com/aryanp45/studymaterialservice/StudyMaterialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aryanp45.studymaterialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMaterialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMaterialServiceApplication.class, args);
+	}
+
+}
---
+++ study-material-service/src/test/java/com/aryanp45/studymaterialservice/StudyMaterialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryanp45.studymaterialservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMaterialServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

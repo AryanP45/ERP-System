c39597c010fb7bc108957fc4317c7b8c200f6ae3
Aryan Patil <aryanitinpatil@gmail.com>
2024-04-21 19:57:14 +0530

    dept enum for faculty

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 College ERP in microservices
+
+doc : https://www.notion.so/aryanp45/ERP-system-design-6ee544bea2324d28b0ac208071b4f4fc
---
+++ faculty-service/src/main/java/com/aryanp45/facultyservice/enums/Department.java
@@ -0,0 +1,10 @@
+package com.aryanp45.facultyservice.enums;
+
+public enum Department {
+	COMPUTER,
+	IT,
+	ENTC,
+	AIDS,
+	MECH,
+	CIVIL
+}

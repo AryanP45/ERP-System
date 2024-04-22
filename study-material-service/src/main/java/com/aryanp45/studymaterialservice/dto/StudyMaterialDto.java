111cffc0075928a4c811848c39b19e13cdbe87ee
Aryan Patil <aryanitinpatil@gmail.com>
2024-04-22 10:20:08 +0530

    Create StudyMaterialDto.java

---
+++ study-material-service/src/main/java/com/aryanp45/studymaterialservice/dto/StudyMaterialDto.java
@@ -0,0 +1,14 @@
+package com.aryanp45.studymaterialservice.dto;
+
+import java.util.Date;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class StudyMaterialDto {
+	private String title;
+
+	private List<String> files;
+
+	private Date timestamp;
+}

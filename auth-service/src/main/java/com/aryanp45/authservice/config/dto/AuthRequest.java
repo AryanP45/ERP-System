104407d40b1dd3cf2c1d329423abe897ecf3b764
Aryan Patil <aryanitinpatil@gmail.com>
2024-04-29 03:53:59 +0530

    Create AuthRequest.java
    dto

---
+++ auth-service/src/main/java/com/aryanp45/authservice/config/dto/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.aryanp45.authservice.config.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
